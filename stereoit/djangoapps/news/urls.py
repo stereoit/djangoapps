20e7e0eff704b10e2f78557357ea52223e207f90
Robert Smol <robert.smol@stereoit.com>
2009-10-11 23:37:34 +0200

    Initial import of the DjangoApps project.
    stereoit.djangoapps project includes the code we reuse for many customers.
    At the moment it only consists 'News' application as this is quite common
    request among customers.

---
+++ stereoit/djangoapps/news/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
+from django.views.generic.list_detail import object_list
+
+
+from stereoit.djangoapps.news.models import News
+
+
+news_dict = {
+	'queryset' : News.objects.all()
+}
+
+urlpatterns = patterns('stereoit.djangoapps.news.views',
+#     (r'show/1/$', 'news_test'), #show news detail
+	(r'^$', direct_to_template, {'template': 'news/index.html'}),
+	(r'^show/$', object_list, dict(
+				news_dict, 
+				paginate_by=20,
+				template_object_name='news',
+			)),
+    url(r'^(?P<slug>[-\w]+)/$', 'detail', name="news_detail"), #show news detail
+)
