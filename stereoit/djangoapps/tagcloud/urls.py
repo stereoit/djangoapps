63a3af731cd423b552d1e45226e9b7af10a8c57a
Robert Smol <robert.smol@stereoit.com>
2009-11-08 23:37:50 +0100

    AJAX support landed, only basic yet.
    Added urls and views for serving the description about the tag via AJAX.

--- stereoit/djangoapps/tagcloud/urls.py
+++ stereoit/djangoapps/tagcloud/urls.py
@@ -10,6 +10,8 @@ tags_dict = {
 
 urlpatterns = patterns('stereoit.djangoapps.tagcloud.views',
 	(r'^$', direct_to_template, {'template': 'tagcloud/index.html'}),
+    url(r'ajax/description/(?P<slug>[-\w]+)/$', 'tag_tooltip_description', name="ajax-tag-description"),
+    url(r'ajax/(?P<slug>[-\w]+)/$', 'tag_tooltip', name="ajax-tag"), #show tag detail
     url(r'^cloud/(?P<slug>[-\w]+)/$', 'tagcloud_detail', name="tagcloud_detail"), #show the coud detail
     url(r'(?P<slug>[-\w]+)/$', 'tag_detail', name="tag_detail"), #show tag detail
 )
